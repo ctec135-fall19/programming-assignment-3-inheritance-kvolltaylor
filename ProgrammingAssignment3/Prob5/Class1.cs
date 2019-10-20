91668037d7853b8961ece52976cb4ae179c345c5
Katrina <k.voll-taylor@students.clark.edu>
2019-10-20 13:04:51 -0700

    Prob2 complete, Prob5 wip

---
+++ ProgrammingAssignment3/Prob5/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    public class Class1 : IComparable
+    {
+        #region fields
+        // create 2 fields for use with this class
+        private int intNumber;
+        private string strProduce;
+
+        public Class1(int Number, string Produce)
+        {
+            intNumber = Number;
+            strProduce = Produce;
+        }
+        #endregion
+
+        #region methods
+        // create methods for both fields' accessors and mutators
+        public int Number
+        {
+            get { return intNumber; }
+            set { intNumber = value; }
+        }
+        public string Produce
+        {
+            get { return strProduce; }
+            set { strProduce = value; }
+        }
+        #endregion
+
+        #region nested classes : IComparer
+
+        // nested classes to use for sorting both fields
+        // using IComparer to create multiple methods for sorting
+
+        // nested class for sorting strings in strProduce field in alphabetic order
+        public class sortProduceDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Class1 obj1 = (Class1)a;
+                Class1 obj2 = (Class1)b;
+                return String.Compare(obj2.strProduce, obj1.strProduce);
+            }
+        }
+
+        // nested class for sorting ints in intNumber field in descending order
+        public class sortNumberDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Class1 obj1 = (Class1)a;
+                Class1 obj2 = (Class1)b;
+                if (obj1.intNumber < obj2.intNumber)
+                    return 1;
+                if (obj1.intNumber > obj2.intNumber)
+                    return -1;
+                else
+                    return 0;
+
+            }
+        }
+        #endregion
+
+        #region methods for IComparable use 
+        // Implement IComparable CompareTo to provide default sort order.
+        int IComparable.CompareTo(object obj)
+        {
+            Class1 compare = (Class1)obj;
+            return String.Compare(this.Produce, compare.Produce);
+        }
+
+        //Method to return IComparer object for sort helper.
+        public static IComparer sortProduceDescending()
+        {
+            return (IComparer)new sortProduceDescendingHelper();
+        }
+
+        //Method to return IComparer object for sort helper.
+        public static IComparer sortNumberDescending()
+        {
+            return (IComparer)new sortNumberDescendingHelper();
+        }
+
+        
+
+        #endregion
+
+    }
+}
---
+++ ProgrammingAssignment3/Prob5/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+
+Author: Katrina Voll-Taylor
+Team: Torin Tashima and Katrina Voll-Taylor
+Date: 21 October 2019
+CTEC 135: Microsoft Software Development with C#
+Module 4, Programming Assignment 3, Problem 5
+
+    Prob 5: 
+
+        - Create a class with two fields, one a string and the other an int. 
+          The class implements the IComparable interface.
+        - Create appropriate methods for the class
+        - Objects in the class should be ordered in the following manner:
+            - primary sort order is alphabetical according to the text field
+            - secondary sort order is largest first according to the int field
+        - Demonstrate the the objects can be sorted according to requirement
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of objects
+            // pass values into each field using their set method
+            Class1[] objects = new Class1[5];
+            new Class1 (42, "banana" );
+            new Class1(5, "endive" );
+            new Class1(24, "apple" );
+            new Class1(777, "carrot" );
+            new Class1(124, "daikon" );
+
+            // Display the array of objects unsorted
+            Console.WriteLine("The array of objects unsorted:\n");
+            foreach(Class1 obj in objects)
+            {
+                Console.Write(obj.Number);
+                Console.WriteLine(" - " + obj.Produce);
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 55) + "\n\n");
+
+            // Display the array of objects sorted by Number
+            // Sorted by descending value
+            
+            // Display the array of objects sorted by Produce
+            // Sorted by alphabetic order
+
+        }
+    }
+}
